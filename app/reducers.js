4cb05782a399d921518756bde6bb1e994ad56882
Heesu Suh <hssuh96@gmail.com>
2017-11-12 22:02:59 +0900

    add global store

--- app/reducers.js
+++ app/reducers.js
@@ -7,6 +7,7 @@ import { combineReducers } from 'redux-immutable';
 import { fromJS } from 'immutable';
 import { LOCATION_CHANGE } from 'react-router-redux';
 
+import globalReducer from 'global/reducer';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
 /*
@@ -44,6 +45,7 @@ export default function createReducer(asyncReducers) {
   return combineReducers({
     route: routeReducer,
     language: languageProviderReducer,
+    global: globalReducer,
     ...asyncReducers,
   });
 }
