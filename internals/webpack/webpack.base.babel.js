9d68860beda4d599c28114bc0af5690061d733cc
onemonster <onemonsterceo@gmail.com>
2018-01-07 22:26:44 +0900

    Added API_HOST to .env

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -72,6 +72,7 @@ module.exports = (options) => ({
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_HOST: JSON.stringify(process.env.API_HOST),
       },
     }),
     new webpack.NamedModulesPlugin(),
