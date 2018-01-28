f1a54c43c419a11501f98e1cfca7ebbe32f7d0e6
chan-wookKim <cksdnr987@wafflestudio.com>
2018-01-28 19:50:55 +0900

    Normalized

--- app/containers/LectureDetailPage/sagas.js
+++ app/containers/LectureDetailPage/sagas.js
@@ -1,13 +1,15 @@
 import api from 'services/api';
+import normalize from 'json-api-normalizer';
+import build from 'redux-object';
 import { takeLatest, call, put } from 'redux-saga/effects';
 import { Types, Creators as Actions } from './reducer';
 
-export function* getLectureDetail({ data }) {
-  const { lectureId } = data;
+export function* getLectureDetail({ data: actionData }) {
+  const { lectureId } = actionData;
   const response = yield call(api.getLectureDetail, lectureId);
   if (response.ok) {
     const { data } = response;
-    console.log(data);
+    yield put(Actions.getLectureDetailSuccess(normalize(data)));
   } else {
     yield put(Actions.getLectureDetailFailure());
   }
