6b88db457b9f2026fb8e6d26d4eadce9a9921d87
yks1502 <516yks@naver.com>
2018-03-04 18:52:02 +0900

    delete connect

--- app/containers/SignUpCompletePage/index.js
+++ app/containers/SignUpCompletePage/index.js
@@ -12,35 +12,29 @@ import {
   ReturnLink,
 } from './index.style';
 
-export class SignUpCompletePage extends React.PureComponent<any> {
-  render() {
-    return (
-      <Background>
-        <Helmet
-          title="SignUpCompletePage"
-          meta={[
-            { name: 'description', content: 'Description of SignUpCompletePage' },
-          ]}
-        />
-        <Logo />
-        <WaitingText>
-          {messages.waiting}
-        </WaitingText>
-        <WebMailText>
-          {messages.webMailSent}
-          <br />
-          {messages.checkMail}
-        </WebMailText>
-        <ReturnLink to="/">
-          <ReturnButton>
-            <ReturnText>
-              {messages.returnText}
-            </ReturnText>
-          </ReturnButton>
-        </ReturnLink>
-      </Background>
-    );
-  }
-}
-
-export default SignUpCompletePage;
+export default () => (
+  <Background>
+    <Helmet
+      title="SignUpCompletePage"
+      meta={[
+        { name: 'description', content: 'Description of SignUpCompletePage' },
+      ]}
+    />
+    <Logo />
+    <WaitingText>
+      {messages.waiting}
+    </WaitingText>
+    <WebMailText>
+      {messages.webMailSent}
+      <br />
+      {messages.checkMail}
+    </WebMailText>
+    <ReturnLink to="/">
+      <ReturnButton>
+        <ReturnText>
+          {messages.returnText}
+        </ReturnText>
+      </ReturnButton>
+    </ReturnLink>
+  </Background>
+);
