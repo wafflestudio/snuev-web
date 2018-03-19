37acab945c7631901c218773a94ec57074338908
yks1502 <516yks@naver.com>
2018-03-19 15:11:55 +0900

    updated departments to sign up page

--- app/containers/SignUpPage/selectors.js
+++ app/containers/SignUpPage/selectors.js
@@ -1,5 +1,10 @@
 const makeSelectPage = () => (state) => state.get('signUpPage');
 
+const makeSelectDepartments = () => createSelector(
+  makeSelectEntities(),
+  (entities) => denormalize(entities, 'departments', null),
+);
+
 export {
   makeSelectPage,
 };
