416db2d33e86521f86ab44506f3e991e8753c935
yks1502 <516yks@naver.com>
2018-03-19 17:01:52 +0900

    updated sign in with router

--- app/global/sagas.js
+++ app/global/sagas.js
@@ -1,4 +1,5 @@
 import { take, call, put } from 'redux-saga/effects';
+import { browserHistory } from 'react-router';
 import { Types, Creators as Actions } from './reducer';
 import { request, authRequest } from '../services/api';
 import { setAuthToken, clearAuthToken } from '../services/localStorage';
@@ -36,6 +37,11 @@ export function* signIn({ username, password }) {
     yield put(Actions.signInSuccess(response.data));
     setAuthToken(response.data.meta.auth_token);
     yield call(userInformation);
+    if (document.referrer && document.referrer.indexOf('localhost') !== -1) {
+      history.back();
+    } else {
+      browserHistory.push('/');
+    }
   } catch (error) {
     yield put(Actions.signInFailure(error.errors));
   }
@@ -47,6 +53,7 @@ export function* userInformation() {
     yield put(Actions.userSuccess(response.data.data.id));
     yield put(Actions.normalizeData(response.data));
   } catch (error) {
+    yield put(Actions.signOut());
     yield put(Actions.userFailure(error.errors));
   }
 }
