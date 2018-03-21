1fb03a546df2e3dcf89f76ece4e88ef80f5ff750
yks1502 <516yks@naver.com>
2018-03-21 14:40:13 +0900

    implemented resend email button

--- app/containers/ConfirmEmailPage/reducer.js
+++ app/containers/ConfirmEmailPage/reducer.js
@@ -1,10 +1,3 @@
-/*
- *
- * ConfirmEmailPage reducer
- *
- */
-
-
 import { createReducer, createActions } from 'reduxsauce';
 import { fromJS } from 'immutable';
 
@@ -14,36 +7,55 @@ export const { Types, Creators } = createActions({
   confirmEmailRequest: ['data'],
   confirmEmailSuccess: null,
   confirmEmailFailure: ['error'],
+  resendEmailRequest: null,
+  resendEmailSuccess: null,
+  resendEmailFailure: ['error'],
 });
 
 
 /* ------------- Initial State ------------- */
 
 export const initialState = fromJS({
-  data: null,
-  isFetching: true,
-  success: false,
-  error: null,
+  confirmEmail: {
+    data: null,
+    isFetching: false,
+    success: null,
+    error: null,
+  },
+  resendEmail: {
+    isFetching: false,
+    success: false,
+    error: null,
+  },
 });
 
 /* ------------- Reducers ------------- */
 
-// request the data from an api
-export const request = (state, { data }) =>
-  state.merge({ data, isFetching: true, success: false, error: null });
+export const confirmEmailRequest = (state, { data }) =>
+  state.mergeDeep({ confirmEmail: { data, isFetching: true, success: false, error: null } });
 
-// successful api lookup
-export const success = (state) =>
-  state.merge({ isFetching: false, success: true, error: null });
+export const confirmEmailSuccess = (state) =>
+  state.mergeDeep({ confirmEmail: { isFetching: false, success: true, error: null } });
 
-// Something went wrong somewhere.
-export const failure = (state) =>
-  state.merge({ isFetching: false, success: false, error: true });
+export const confirmEmailFailure = (state, { error }) =>
+  state.mergeDeep({ confirmEmail: { isFetching: false, success: false, error } });
+
+export const resendEmailRequest = (state) =>
+  state.mergeDeep({ resendEmail: { isFetching: true, success: false, error: null } });
+
+export const resendEmailSuccess = (state) =>
+  state.mergeDeep({ resendEmail: { isFetching: false, success: true, error: null } });
+
+export const resendEmailFailure = (state, { error }) =>
+  state.mergeDeep({ resendEmail: { isFething: false, success: true, error } });
 
 /* ------------- Hookup Reducers To Types ------------- */
 
 export default createReducer(initialState, {
-  [Types.CONFIRM_EMAIL_REQUEST]: request,
-  [Types.CONFIRM_EMAIL_SUCCESS]: success,
-  [Types.CONFIRM_EMAIL_FAILURE]: failure,
+  [Types.CONFIRM_EMAIL_REQUEST]: confirmEmailRequest,
+  [Types.CONFIRM_EMAIL_SUCCESS]: confirmEmailSuccess,
+  [Types.CONFIRM_EMAIL_FAILURE]: confirmEmailFailure,
+  [Types.RESEND_EMAIL_REQUEST]: resendEmailRequest,
+  [Types.RESEND_EMAIL_SUCCESS]: resendEmailSuccess,
+  [Types.RESEND_EMAIL_FAILURE]: resendEmailFailure,
 });
