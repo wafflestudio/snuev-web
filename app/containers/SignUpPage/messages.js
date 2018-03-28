a7b31db7b2c491e6e12a0e1e6bdf7c81f77cff73
yks1502 <516yks@naver.com>
2018-03-28 18:36:49 +0900

    deleted useless lines

--- app/containers/SignUpPage/messages.js
+++ app/containers/SignUpPage/messages.js
@@ -1,15 +1,6 @@
-/*
- * SignUpPage Messages
- *
- * This contains all the text for the SignUpPage component.
- */
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  header: {
-    id: 'app.containers.SignUpPage.header',
-    defaultMessage: 'This is SignUpPage container !',
-  },
   createAccount: '계정 만들기',
   usernameInputText: '서울대학교 이메일 인증이 필요합니다. 마이스누(mySNU) 계정명과 동일하게 입력해주세요.',
   input: {
