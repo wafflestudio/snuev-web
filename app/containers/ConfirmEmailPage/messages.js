aac914f3586e6a73987f6f1fb82e867ad696ec23
Hoseong Hwang <liam.hwang@buzzvil.com>
2018-09-30 18:40:48 +0900

    Show toasts

---
+++ app/containers/ConfirmEmailPage/messages.js
@@ -0,0 +1,5 @@
+const messages = {
+  'confirmEmailSuccess': '이메일 인증이 완료되었습니다.',
+};
+
+export default messages;
--- app/containers/ConfirmEmailPage/sagas.js
+++ app/containers/ConfirmEmailPage/sagas.js
@@ -1,6 +1,9 @@
+import { browserHistory } from 'react-router';
 import { take, call, put } from 'redux-saga/effects';
 import { request, authRequest } from 'services/api';
+import messages from './messages';
 import { Types, Creators as Actions } from './reducer';
+import { Creators as GlobalActions } from 'global/reducer';
 
 export function* watchConfirmEmailRequest() {
   while (true) {
@@ -13,6 +16,8 @@ export function* confirmEmail(token) {
   try {
     yield request.put(`/v1/user/confirm_email?confirmation_token=${token}`);
     yield put(Actions.confirmEmailSuccess());
+    yield put(GlobalActions.showToast(messages.confirmEmailSuccess));
+    browserHistory.push('/');
   } catch (error) {
     yield put(Actions.confirmEmailFailure(error.errors));
   }
