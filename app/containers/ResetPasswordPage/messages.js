06023ef47de36e8f921f38f4316d6db311c2f2e5
onemonster <onemonsterceo@gmail.com>
2018-10-11 02:44:46 +0900

    redirect to main page on reset password

--- app/containers/ResetPasswordPage/messages.js
+++ app/containers/ResetPasswordPage/messages.js
@@ -5,4 +5,5 @@ export default defineMessages({
   input: { usernameHint: '마이스누 계정' },
   usernameInputText: '서울대학교 이메일 인증이 필요하므로<br />마이스누 계정과 동일하게 입력해주세요.',
   confirmation: '인증',
+  toast: '마이스누 계정으로 비밀번호 재설정 이메일을 보냈습니다',
 });
